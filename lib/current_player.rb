df8555a4afa5229731c5cb5f2d1fce969b3d76cf
Mona Javed Syed <monajaved93@gmail.com>
2020-10-19 06:48:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   count = 0
   board.each do |value|
     # if board[value] == "X" || board[value] == "O"
-    #   count += 1
+      count += 1
     # end
   end
   return count
